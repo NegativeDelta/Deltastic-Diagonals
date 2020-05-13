38e3baabad4bc15ad4008f9735c821a6c8262e3e
Negative Delta <61510683+NegativeDelta@users.noreply.github.com>
2020-05-13 09:04:04 +0200

    Update diagonals.cs

--- diagonals.cs
+++ diagonals.cs
@@ -11,35 +11,44 @@ public class Program {
         string type = Console.ReadLine ();
         Console.WriteLine (" ");
         switch (type) {
+
             case "A":
-                Console.WriteLine ("Screen's diagonal [ inches ]: ");
-                float diagonal = float.Parse (Console.ReadLine ());
-                Console.WriteLine (" ");
-                Console.WriteLine ("First ratio: ");
-                float xA = float.Parse (Console.ReadLine ());
-                Console.WriteLine (" ");
-                Console.WriteLine ("Second ratio: ");
-                float yA = float.Parse (Console.ReadLine ());
-                Console.WriteLine (" ");
-                float part = (diagonal * diagonal) / ((xA * xA) + (yA * yA));
-                float newX = Convert.ToSingle (Math.Sqrt (part * xA * xA));
-                float newY = Convert.ToSingle (Math.Sqrt (part * yA * yA));
-                Console.WriteLine ("First screen dimension: " + Math.Round (newX, 1) + " inches; " + Math.Round (newX * 2.54f, 1) + " cm");
-                Console.WriteLine ("Second screen dimension: " + Math.Round (newY, 1) + " inches; " + Math.Round (newY * 2.54f, 1) + " cm");
-                string analphabet = Console.ReadLine ();
+                Dimensions();
                 break;
+
             case "B":
-                Console.WriteLine ("The first dimension [ cm ]: ");
-                float xB = float.Parse (Console.ReadLine ());
-                Console.WriteLine (" ");
-                Console.WriteLine ("The second dimension [ cm ]: ");
-                float yB = float.Parse (Console.ReadLine ());
-                Console.WriteLine (" ");
-                Console.WriteLine ("This screen's diagonal is " + Math.Round (Math.Sqrt (xB * xB + yB * yB) / 2.54f, 1) + " inches ( " + Math.Round (Math.Sqrt (xB * xB + yB * yB), 1) + " cm )");
-                string bitchlasaga = Console.ReadLine ();
+                Diagonal();
                 break;
         }
 
     }
-
+    private void Dimensions()
+    {
+        Console.WriteLine("Screen's diagonal [ inches ]: ");
+        float diagonal = float.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+        Console.WriteLine("First ratio: ");
+        float xA = float.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+        Console.WriteLine("Second ratio: ");
+        float yA = float.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+        float part = (diagonal * diagonal) / ((xA * xA) + (yA * yA));
+        float newX = Convert.ToSingle(Math.Sqrt(part * xA * xA));
+        float newY = Convert.ToSingle(Math.Sqrt(part * yA * yA));
+        Console.WriteLine("First screen dimension: " + Math.Round(newX, 1) + " inches; " + Math.Round(newX * 2.54f, 1) + " cm");
+        Console.WriteLine("Second screen dimension: " + Math.Round(newY, 1) + " inches; " + Math.Round(newY * 2.54f, 1) + " cm");
+        string analphabet = Console.ReadLine();
+    }
+    private void Diagonal()
+    {
+        Console.WriteLine("The first dimension [ cm ]: ");
+        float xB = float.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+        Console.WriteLine("The second dimension [ cm ]: ");
+        float yB = float.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+        Console.WriteLine("This screen's diagonal is " + Math.Round(Math.Sqrt(xB * xB + yB * yB) / 2.54f, 1) + " inches ( " + Math.Round(Math.Sqrt(xB * xB + yB * yB), 1) + " cm )");
+        string bitchlasaga = Console.ReadLine();
+    }
 }
